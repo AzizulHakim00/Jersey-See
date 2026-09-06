f502e09f2e5d7a4a707472c4c80b0b4d5d21a502
Azizul Hakim Omor <157488702+AzizulHakim00@users.noreply.github.com>
2026-09-06 09:49:57 +0600

    test: target the homepage hero artwork contract

--- src/test/java/bd/edu/seu/jerseysee/controller/HomeHeroMediaContractTest.java
+++ src/test/java/bd/edu/seu/jerseysee/controller/HomeHeroMediaContractTest.java
@@ -9,10 +9,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 class HomeHeroMediaContractTest {
 
     @Test
-    void homepageUsesThePremiumFootballerArtworkInsteadOfAProductPlaceholder() throws Exception {
+    void homepageUsesThePremiumFootballerArtworkInTheHero() throws Exception {
         String home = Files.readString(Path.of("src/main/resources/templates/home/index.html"));
 
-        assertThat(home).contains("/images/auth-footballer.svg");
-        assertThat(home).doesNotContain("class=\"premium-hero-media\"", "'/images/product-placeholder.svg'");
+        assertThat(home).contains("class=\"premium-hero-media\"", "th:src=\"@{/images/auth-footballer.svg}\"");
+        assertThat(home).doesNotContain("'/images/hero-kit.svg'");
     }
 }
